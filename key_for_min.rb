4d3d2b09255f3832dd4c3e46bd7f733578da2122
Oswaldo Ortiz <oswaldoortiz0519@gmail.com>
2020-08-25 16:29:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 1000
+  sym = :some_key
+  name_hash.each do |key, value|
+    if value < min
+      sym = key
+    end 
+  end
+  return sym 
 end
